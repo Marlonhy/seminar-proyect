ca41332ccf388bbcf6493e6a2af68e07315300dd
Marlon Enrique Hernandez Yepez <marlon.hernandez@viajar.intra>
2026-01-31 12:20:14 -0500

    31/01/2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'menu/home',
+    pathMatch: 'full'
+  },
+  {
     path: 'intro',
     loadComponent: () =>
       import('./intro/intro.page').then(m => m.IntroPage)
@@ -14,18 +19,19 @@ export const routes: Routes = [
       import('./login/login.page').then(m => m.LoginPage)
   },
   {
-    path: 'home',
-    loadComponent: () =>
-      import('./home/home.page').then(m => m.HomePage),
-    canActivate: [IntroGuard, AuthGuard]
-  },
-  {
-    path: '',
-    redirectTo: 'intro',
-    pathMatch: 'full'
-  },
-  {
     path: 'register',
     loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'menu',
+    loadComponent: () => import('./menu/menu.page').then( m => m.MenuPage),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.page').then(m => m.HomePage),
+        canActivate: [IntroGuard, AuthGuard]
+      },
+    ]
   }
 ];
---
+++ src/app/menu/menu.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+  standalone: true,
+  imports: [ CommonModule, FormsModule, IonicModule ]
+})
+export class MenuPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  goToIntro() {
+    console.log('Going to intro page');
+  }
+}
